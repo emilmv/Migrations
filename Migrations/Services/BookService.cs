7481b728eb7fe39d9b9ca83e2e444f7c8f8e0fe1
emilmv <emilnm@code.edu.az>
2024-04-23 10:21:52 +0400

    BookService Added

---
+++ Migrations/Services/BookService.cs
@@ -0,0 +1,38 @@
+﻿using Migrations.DAL;
+using Migrations.Entities;
+
+namespace Migrations.Services
+{
+    internal class BookService
+    {
+        public void Add(Book book) //Create
+        {
+            using var dc = new AppDbContext();
+            var exists = dc.Books.FirstOrDefault(b => b.Name.Equals(book.Name, StringComparison.OrdinalIgnoreCase));
+            if (exists != null) return;
+            dc.Books.Add(book);
+        }
+        public List<Book> GetAll() //Read
+        {
+            using var dc = new AppDbContext();
+            return dc.Books.ToList();
+        }
+        public Book GetById(int id) //Read
+        {
+            using var dc = new AppDbContext();
+            return dc.Books.FirstOrDefault(b => b.ID == id);
+        }
+        public void Update(int id, decimal newPrice) //Update
+        {
+            using var dc = new AppDbContext();
+            var exists = dc.Books.FirstOrDefault(b => b.ID==id);
+            if (exists != null) exists.Price = newPrice;
+        }
+        public void Delete(int id) //Delete
+        {
+            using var dc = new AppDbContext();
+            var exists = dc.Books.FirstOrDefault(b => b.ID == id);
+            if (exists != null) dc.Books.Remove(exists);
+        }
+    }
+}
