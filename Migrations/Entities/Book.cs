d03bb95ee8ed9462a8b26f4daed74d2d01e4c842
emilmv <emilnm@code.edu.az>
2024-04-23 09:43:41 +0400

    Added Tables

--- Migrations/Entities/Book.cs
+++ Migrations/Entities/Book.cs
@@ -6,6 +6,7 @@ namespace Migrations.Entities
     internal class Book
     {
         [Key] public int ID { get; set; }
-
+        [Required] public string Name { get; set; }
+        [Required] public decimal Price { get; set; }
     }
 }
--- Migrations/Entities/Library.cs
+++ Migrations/Entities/Library.cs
@@ -1,7 +1,13 @@
 ﻿
+using System.ComponentModel.DataAnnotations;
+
 namespace Migrations.Entities
 {
     internal class Library
     {
+        [Key] public int ID { get; set; }
+        [Required] public string Name { get; set; }
+        public List<Book>Books { get; set; }
+
     }
 }
